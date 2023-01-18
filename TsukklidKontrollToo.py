79b4350a916fa3435311948b3a67a21a5e140fe5
opilane <opilane@HP-CZC2349HTB>
2023-01-18 12:20:04 +0200

    Add project files.

---
+++ TsukklidKontrollToo.py
@@ -0,0 +1,42 @@
+from random import *
+
+
+#cat = ("\n  ^---^\n ( o o )\n  ! = !/)")
+#while True:
+#    try:
+#        catnumber = int(input("Tere! Kui palju kassid sa tahad? Vali number 1-9:"))
+#        break
+#    except ValueError:
+#        print("See pole täisarv")
+#if catnumber<1 or catnumber>9:
+#    print("Vali õige number")
+#else:
+#    x = 0
+#    while x != catnumber:
+#        print(end="\n  ^---^\n ( o o )\n  ! = !/)")
+#        x += 1
+
+
+#while True:
+#    try:
+#        number = int(input("Tere! Sisesta number:"))
+#        kraad = int(input("Sisesta kraad:"))
+#        break
+#    except ValueError:
+#        print("See pole täisarv")
+#ans = number**kraad
+#print("Siin on sinu vastus:", ans)
+
+while True:
+    try:
+        yesno = str(input("Tere! Kas sa tahad alustada programmi?  jah või ei"))
+        break
+    except ValueError:
+        print("Vali õige vastu")
+if yesno == "jah":
+    student = randint(1,30)
+    for x in range (student):
+        hinne = 
+
+else:
+    print("Head aega!")
